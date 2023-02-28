eb9f0950074551a5db846c9039d7ddba87208b8c
leo-carrey <leo.carrey@laplateforme.io>
2023-02-28 13:51:30 +0100

    [FIX] fix erreur

--- job02.py
+++ job02.py
@@ -1,29 +1,29 @@
 class Livre:
     def __init__(self, titre, auteur, nombre_depages):
-        self._titre = titre
-        self._auteur = auteur
-        self._nombre_depages = nombre_depages
+        self.__titre = titre
+        self.__auteur = auteur
+        self.__nombre_depages = nombre_depages
 
     def modif_titre(self, titre):
-        self._titre = titre
-        return self._titre
+        self.__titre = titre
+        return self.__titre
     
     def modif_auteur(self, auteur):
-        self._auteur = auteur
-        return self._auteur
+        self.__auteur = auteur
+        return self.__auteur
     
     def modif_nombre_depages(self, nombre_depages):
-        self._nombre_depages = nombre_depages
+        self.__nombre_depages = nombre_depages
         if nombre_depages != int(nombre_depages):
-            self._nombre_depages = 0
+            self.__nombre_depages = 0
             print("veuillez inserer un nombre entier positif !!!")
-        return self._nombre_depages
+        return self.__nombre_depages
         
     def afficher(self):
-        return f'le livre se nomme {self._titre}, il a pour auteur {self._auteur} et a {self._nombre_depages} pages'
+        return f'le livre se nomme {self.__titre}, il a pour auteur {self.__auteur} et a {self.__nombre_depages} pages'
     
 livre = Livre("Les miss","Jean",1)
 livre.modif_titre("Les misérables")
 livre.modif_auteur("Jean Valjean")
-print(livre.modif_nombre_depages(288.1))
+print(livre.modif_nombre_depages(288))
 print(livre.afficher())
--- jour02/job01.py
+++ jour02/job01.py
@@ -1,18 +1,18 @@
 class Rectangle:
     def __init__(self, longueur, largueur):
-        self._longueur = longueur
-        self._largeur = largueur
+        self.__longueur = longueur
+        self.__largeur = largueur
 
     def modif_longueur(self, longueur):
-        self._longueur = longueur
-        return self._longueur
+        self.__longueur = longueur
+        return self.__longueur
     
     def modif_largueur(self, largueur):
-        self._largeur = largueur
-        return self._largeur
+        self.__largeur = largueur
+        return self.__largeur
     
     def afficher(self):
-        return f'la longueur du triangle est de {self._longueur} et a une largueur de {self._largeur}'
+        return f'la longueur du triangle est de {self.__longueur} et a une largueur de {self.__largeur}'
     
 rectangle = Rectangle(10,5)
 print(rectangle.modif_longueur(5))
