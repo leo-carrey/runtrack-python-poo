72895ff4b29666267c7f5638ab39b890e89e74b7
leo-carrey <leo.carrey@laplateforme.io>
2023-03-01 09:48:38 +0100

    [ADD] job in jour02

---
+++ jour02/job06.py
@@ -0,0 +1,6 @@
+class Commande:
+    def __init__(self,numero_de_commande,plats_commandes, statut):
+        self.__numero_de_commande = numero_de_commande
+        self.__plats_commandes = plats_commandes
+        self.__statut = statut
+    
